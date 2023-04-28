6dc939358e8fb62ce02d886689d038a20a0f79e1
Marcin Sacha <marcindev03@gmail.com>
2023-04-28 15:25:43 +0200

    Prepare category by filtering

--- web/app/api/products/route.ts
+++ web/app/api/products/route.ts
@@ -2,11 +2,24 @@ import { NextResponse } from "next/server";
 import { prisma } from "@modules/databse";
 import { DatabaseErrorResponse } from "@modules/server";
 
-export const GET = async () => {
+export const GET = async (req: Request) => {
+  console.log("API", "Request URL", req.url);
+
   prisma.$connect();
 
+  let filters = {};
+
+  const { searchParams } = new URL(req.url);
+  const categoryName = searchParams.get("categoryName");
+
+  if (categoryName) {
+    filters = Object.assign(filters, {
+      where: { category: { name: categoryName } },
+    });
+  }
+
   try {
-    const products = await prisma.product.findMany({});
+    const products = await prisma.product.findMany(filters);
 
     return NextResponse.json({ data: products });
   } catch (err) {
