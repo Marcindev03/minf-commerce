22ae0733f857bd3e8f6d9af8021c11e2fd71098d
Marcin Sacha <marcindev03@gmail.com>
2023-05-01 13:10:05 +0200

    Show category name in products list

--- web/modules/products/ProductsList/ProductsList.tsx
+++ web/modules/products/ProductsList/ProductsList.tsx
@@ -3,6 +3,7 @@
 import { FC } from "react";
 import { useProductsQuery } from "@modules/api";
 import { ProductItem } from "../ProductItem";
+import Link from "next/link";
 
 type ProductsListProps = {
   categoryName?: string;
@@ -12,10 +13,20 @@ export const ProductsList: FC<ProductsListProps> = ({ categoryName }) => {
   const { data } = useProductsQuery(categoryName);
 
   return (
-    <section className="grid grid-cols-3 col-span-3 gap-10">
-      {data?.data?.map((product) => (
-        <ProductItem key={product.id} {...product} />
-      ))}
+    <section className="col-span-3">
+      <article>
+        <h3 className="text-2xl pb-4">
+          <Link href="/products" className="hover:underline">
+            Produkty
+          </Link>{" "}
+          {!!categoryName && `| ${categoryName}`}
+        </h3>
+      </article>
+      <section className="grid grid-cols-3  gap-10">
+        {data?.data?.map((product) => (
+          <ProductItem key={product.id} {...product} />
+        ))}
+      </section>
     </section>
   );
 };
