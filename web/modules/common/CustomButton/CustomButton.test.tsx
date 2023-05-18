143fde1afab3870ba01507ee0c5b8517abcbaa0c
Marcin Sacha <marcindev03@gmail.com>
2023-05-18 21:52:53 +0200

    Setup tests

---
+++ web/modules/common/CustomButton/CustomButton.test.tsx
@@ -0,0 +1,47 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { CustomButton } from "./CustomButton";
+
+describe("Custom button", () => {
+  test("should render children", () => {
+    const buttonText = "Click me!";
+    render(<CustomButton>{buttonText}</CustomButton>);
+
+    const button = screen.getByRole("button");
+    expect(button).toHaveTextContent(buttonText);
+  });
+
+  test("should call onClick prop when called", () => {
+    const buttonText = "Click me!";
+    const handleClick = jest.fn();
+    render(<CustomButton onClick={handleClick}>{buttonText}</CustomButton>);
+
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    expect(handleClick).toBeCalledTimes(1);
+  });
+
+  test("should be disabled when disabled props is provided", () => {
+    const buttonText = "Click me!";
+    render(<CustomButton disabled>{buttonText}</CustomButton>);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeDisabled();
+  });
+
+  test("should not fire onClick when disabled", () => {
+    const buttonText = "Click me!";
+    const handleClick = jest.fn();
+    render(
+      <CustomButton disabled onClick={handleClick}>
+        {buttonText}
+      </CustomButton>
+    );
+
+    const button = screen.getByRole("button");
+
+    fireEvent.click(button);
+
+    expect(button).toBeDisabled();
+    expect(handleClick).not.toHaveBeenCalled();
+  });
+});
