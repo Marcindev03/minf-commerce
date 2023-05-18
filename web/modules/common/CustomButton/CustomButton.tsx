38702ddd386fc066b36782fd081a89653318a415
Marcin Sacha <marcindev03@gmail.com>
2023-05-18 21:52:53 +0200

    Setup tests

--- web/modules/common/CustomButton/CustomButton.tsx
+++ web/modules/common/CustomButton/CustomButton.tsx
@@ -1,12 +1,22 @@
 import { FC, ReactNode } from "react";
 
 type CustomButtonProps = {
+  disabled?: boolean;
+  onClick?: () => void;
   children: ReactNode;
 };
 
-export const CustomButton: FC<CustomButtonProps> = ({ children }) => {
+export const CustomButton: FC<CustomButtonProps> = ({
+  disabled,
+  onClick,
+  children,
+}) => {
   return (
-    <button className="bg-blue-500 py-3 px-5 text-white rounded hover:bg-blue-800">
+    <button
+      className="bg-blue-500 py-3 px-5 text-white rounded hover:bg-blue-800"
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
