26669af1bada3d5858e9631da5d69d319f9a6501
Marcin Sacha <marcindev03@gmail.com>
2023-05-30 14:59:25 +0200

    Change rest client url

--- web/modules/api/client/client.ts
+++ web/modules/api/client/client.ts
@@ -4,9 +4,7 @@ import { cache } from "react";
 export const getQueryClient = cache(() => new QueryClient());
 
 export const restClient = async (url: string) => {
-  const baseUrl = `${
-    process.env.VERCEL ? process.env.VERCEL_URL : "http://localhost:4000"
-  }/api`;
+  const baseUrl = `${process.env.VERCEL_URL}/api`;
   const finalUrl = baseUrl + url;
 
   const res = await fetch(finalUrl);
