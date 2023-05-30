2d58e50220e40141fee879f71a2a4aa275e44b05
Marcin Sacha <marcindev03@gmail.com>
2023-05-30 15:06:45 +0200

    env

--- web/modules/api/client/client.ts
+++ web/modules/api/client/client.ts
@@ -6,7 +6,7 @@ export const getQueryClient = cache(() => new QueryClient());
 export const restClient = async (url: string) => {
   console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
 
-  const baseUrl = `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
+  const baseUrl = `${process.env.VERCEL ? "" : "http://localhost:4000"}/api`;
   const finalUrl = baseUrl + url;
 
   const res = await fetch(finalUrl);
