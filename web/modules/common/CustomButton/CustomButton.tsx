5f10feaec262e8c87e2b2bb36c3843e9a5e3fbed
Marcin Sacha <marcindev03@gmail.com>
2023-06-01 13:04:28 +0200

    Feature - Homepage cart link

--- web/modules/common/CustomButton/CustomButton.tsx
+++ web/modules/common/CustomButton/CustomButton.tsx
@@ -5,6 +5,7 @@ import { FC, ReactNode } from "react";
 type CustomButtonProps = {
   href?: string;
   className?: string;
+  variant?: "primary" | "outline";
   disabled?: boolean;
   onClick?: () => void;
   children: ReactNode;
@@ -13,16 +14,19 @@ type CustomButtonProps = {
 export const CustomButton: FC<CustomButtonProps> = ({
   href,
   className,
+  variant = "primary",
   disabled,
   onClick,
   children,
 }) => {
   const Button = () => (
     <button
-      className={classNames(
-        "bg-blue-500 py-3 px-5 text-white rounded hover:bg-blue-800",
-        className
-      )}
+      className={classNames(" py-2 px-5 border rounded", className, {
+        "text-white border-blue-500 bg-blue-500 hover:bg-blue-800 hover:border-blue-800":
+          variant === "primary",
+        "text-black bg-white border-blue-400 hover:bg-blue-400 hover:text-white":
+          variant === "outline",
+      })}
       onClick={onClick}
       disabled={disabled}
     >
