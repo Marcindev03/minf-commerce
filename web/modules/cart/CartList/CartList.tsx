7f01fa3d4797de24489d72904546cd1d7fa1a802
Marcin Sacha <marcindev03@gmail.com>
2023-06-02 09:31:14 +0200

    Feature - Cart mobile design

--- web/modules/cart/CartList/CartList.tsx
+++ web/modules/cart/CartList/CartList.tsx
@@ -4,7 +4,7 @@ import { FC, useCallback, useEffect, useMemo, useState } from "react";
 import { CartItem } from "../CartItem";
 import { getCartItems } from "../helpers";
 import { DeliveryForm } from "@modules/order/DeliveryForm";
-import Link from "next/link";
+import { CartSummary } from "../CartSummary";
 
 type CartListProps = {};
 
@@ -31,6 +31,7 @@ export const CartList: FC<CartListProps> = () => {
     setProductsSums(new Map(productsSums.set(name, sum)));
   };
 
+  // TODO change logic of product delete
   const handleProductDelete = () => getCart();
 
   const orderSum = useMemo(
@@ -39,46 +40,24 @@ export const CartList: FC<CartListProps> = () => {
   );
 
   return (
-    <div className="bg-gray-100 p-20">
-      <h1 className="mb-20 text-center text-2xl font-bold">Koszyk</h1>
-      <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
-        <div className="rounded-lg md:w-2/3">
-          {data?.data.map((product) => (
-            <CartItem
-              product={product}
-              onQuanityChange={handleQuanityChange}
-              onProductDelete={handleProductDelete}
-            />
-          )) ?? <p className="text-xl text-center mt-20">Koszyk jest pusty</p>}
-        </div>
-        <article>
-          <section className="mb-6">
-            <DeliveryForm />
-          </section>
-          <div className="mt-6 rounded-lg border bg-white p-6 shadow-md md:mt-0 w-72">
-            <div className="mb-2 flex justify-between">
-              <p className="text-gray-700">Produkty</p>
-              <p className="text-gray-700">{orderSum} PLN</p>
-            </div>
-            <div className="flex justify-between">
-              <p className="text-gray-700">Dostawa</p>
-              <p className="text-gray-700">15.99 PLN</p>
-            </div>
-            <hr className="my-4" />
-            <div className="flex justify-between">
-              <p className="text-lg font-bold">Total</p>
-              <div className="">
-                <p className="mb-1 text-lg font-bold">{orderSum} PLN</p>
-              </div>
-            </div>
-            <Link href="/cart/order">
-              <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
-                Przejdź dalej
-              </button>
-            </Link>
-          </div>
-        </article>
-      </div>
-    </div>
+    <section className="mx-auto justify-center px-6 lg:py-10 lg:flex lg:justify-around">
+      <article className="rounded-lg lg:w-3/5 lg:mr-6">
+        {data?.data.map((product) => (
+          <CartItem
+            product={product}
+            onQuanityChange={handleQuanityChange}
+            onProductDelete={handleProductDelete}
+          />
+        )) ?? <p className="text-lg text-center mt-20">Koszyk jest pusty</p>}
+      </article>
+      <article className="md:flex lg:flex-col lg:w-2/5">
+        <section className="mb-6 md:mr-6 md:w-1/2 lg:w-full">
+          <DeliveryForm />
+        </section>
+        <section className="md:w-1/2 lg:w-full">
+          <CartSummary orderPrice={orderSum} deliveryPrice={15.99} />
+        </section>
+      </article>
+    </section>
   );
 };
